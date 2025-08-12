c93f9d08c2c014cf083b2ea0b6382bf200c6bb2f
n4er <nasser432243@gmail.com>
2025-08-12 21:00:42 +0400

    added the models that represent the entities and their tables with the coresponding attributes

---
+++ backend/BackendBase/src/main/java/com/Rihal/BackendBase/model/Itinerary.java
@@ -0,0 +1,29 @@
+package com.AutomatedTravelApp.travel.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "itineraries")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor @Builder
+@ToString(exclude = "segments")
+@EqualsAndHashCode(exclude = "segments")
+public class Itinerary {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String destination;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @OneToMany(mappedBy = "itinerary", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<TripSegment> segments = new ArrayList<>();
+}
