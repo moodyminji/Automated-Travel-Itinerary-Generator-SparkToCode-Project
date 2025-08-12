718dad9bec39f76a0d468aa9330db68ec00a1f3c
Razan Al Raiisi <razanraiisi@gmail.com>
2025-08-12 16:38:01 +0400

    feat: spring boot backend running + added a DemoApi for checking

---
+++ backend/BackendBase/src/test/java/com/Rihal/BackendBase/AutomatedTravelItineraryGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rihal.BackendBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedTravelItineraryGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
