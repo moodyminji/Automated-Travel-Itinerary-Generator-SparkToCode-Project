80892b69f8f72bc1e407dac0f2b791cccbb311ed
Razan Al Raiisi <razanraiisi@gmail.com>
2025-08-17 15:21:22 +0400

    setting up test files

--- backend/BackendBase/src/test/java/com/AutomatedTravelApp/travel/TravelApplicationTests.java
+++ backend/BackendBase/src/test/java/com/AutomatedTravelApp/travel/TravelApplicationTests.java
@@ -2,8 +2,15 @@ package com.AutomatedTravelApp.travel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+
+
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = Replace.ANY)
 class TravelApplicationTests {
 
 	@Test
