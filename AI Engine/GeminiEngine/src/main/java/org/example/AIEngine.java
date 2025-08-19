6c6c97106640712104036b2b14946c1dbc20aeee
Qais Al Raisi <23-0622@student.gutech.edu.om>
2025-08-20 01:31:50 +0400

    added ai engine folder (env removed till confirmation)

---
+++ AI Engine/GeminiEngine/src/main/java/org/example/AIEngine.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+import 
+
+
+public class AIEngine {
+        public static void main(String[] args) {
+            Client client = new Client();
+
+            GenerateContentResponse response =
+                    client.models.generateContent(
+                            "gemini-2.5-flash",
+                            "Explain how AI works in a few words",
+                            null);
+
+            System.out.println(response.text());
+        }
+        }
+
